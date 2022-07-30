39fd2ec34b87353c6e44f93e251e84b3b086bc53
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2022-07-30 17:59:56 +0600

    Add files via upload

---
+++ Tahsin Modified GUI/Start.java
@@ -0,0 +1,10 @@
+import javax.swing.*;  
+import java.awt.event.*; 
+
+public class Start{
+
+      public static void main(String[] args) {
+            new LoginPage();
+            //new HomePage();
+      }
+}
